aa17c943b29905cdf22144a2d9b62df229531cec
peturdainn <peter.dhoye@gmail.com>
2026-02-24 22:59:45 +0100

    version 0.3 of show.php

--- files/show.php
+++ files/show.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Longitude - a tiny Latitude replacement by peturdainn</title>
+    <title>Longitude</title>
 	<meta http-equiv="refresh" content="300">
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <meta charset="utf-8">
     <!-- OpenFreeMap and MapLibre stuff -->
     <script src="https://unpkg.com/maplibre-gl/dist/maplibre-gl.js"></script>
     <link href="https://unpkg.com/maplibre-gl/dist/maplibre-gl.css" rel="stylesheet" />    
-    <!-- --- -->
-
+    <!-- --- -->    
     <style type="text/css">
         html, body {
             overflow:hidden;
@@ -29,13 +28,10 @@
    </style>    
   </head>
   <body>
-  
 <script>
 <?php
 
-// this file (c) 2025 peturdainn
-// MIT licensed
-// latest version at https://github.com/peturdainn/Longitude
+date_default_timezone_set('Europe/Brussels');
 
 // some preparation: init vars and create empty arrays
 echo "var who = [];\n";
@@ -48,13 +44,27 @@ error_reporting('E_ALL');
 $wholist = stripslashes($_GET['who']);
 $who = explode(",", $wholist);
 $count = count($who);
-$zoom = stripslashes($_GET['zoom']);
+$today = date('Y-m-d');
 
 echo "var cnt = ".$count.";\n";
+
 if(isset($_GET['zoom']))
-	echo "var zoomlevel = ".$zoom."\n";
+{
+    $zoom = stripslashes($_GET['zoom']);
+	echo "var zoomset = true\n";
+ 	echo "var zoomlevel = ".$zoom."\n";
+}
 else
+{
+	echo "var zoomset = false\n";
 	echo "var zoomlevel = 15\n";
+}
+
+// for bounding box calculation
+$lat_min = 0;
+$lat_max = 0;
+$lon_min = 0;
+$lon_max = 0;
 
 for ($i = 0; $i < $count; $i++)
 {
@@ -76,49 +86,84 @@ for ($i = 0; $i < $count; $i++)
 		{
 			$speed[$i] = $speed[$i]." km/h";
 		}
+		
+	    if(0 == $i)
+	    {
+	        $lat_min = $lat[$i];
+	        $lat_max = $lat[$i];
+	        $lon_min = $lon[$i];
+	        $lon_max = $lon[$i];
+	    }
+	    else
+	    {
+	        if($lat[$i] < $lat_min) { $lat_min = $lat[$i]; }	    
+	        if($lat[$i] > $lat_max) { $lat_max = $lat[$i]; }	    
+	        if($lon[$i] < $lon_min) { $lon_min = $lon[$i]; }	    
+	        if($lon[$i] > $lon_max) { $lon_max = $lon[$i]; }	    
+	    }
+			    
+	    $pic[$i] = "./data/".$who[$i]."/".$who[$i].".png";
+
+	    if($handle = fopen($pic[$i], "r"))
+	    {
+		    fclose($handle);
+	    }
+	    else
+	    {
+		    $pic[$i] = "longitude.png";
+        }		
+
+	    // stuff those arrays
+	    echo "who.push(\"".$who[$i]."\");\n";
+	    echo "lat.push(".$lat[$i].");\n";
+	    echo "lon.push(".$lon[$i].");\n";
+	    echo "pic.push(\"".$pic[$i]."\");\n";
 	}
-
-	$pic[$i] = "./data/".$who[$i]."/".$who[$i].".png";
-
-	if($handle = fopen($pic[$i], "r"))
-		fclose($handle);
-	else
-		$pic[$i] = "longitude.png";
-
-	// stuff those arrays
-	echo "who.push(\"".$who[$i]."\");\n";
-	echo "lat.push(".$lat[$i].");\n";
-	echo "lon.push(".$lon[$i].");\n";
-	echo "pic.push(\"".$pic[$i]."\");\n";
 }
-
+$latavg = ($lat_min + $lat_max) / 2;
+$lonavg = ($lon_min + $lon_max) / 2;
+echo "latavg = ".$latavg.";\n";
+echo "lonavg = ".$lonavg.";\n";
+echo "var bbox = [[".$lon_min.", ".$lat_min."], [".$lon_max.", ".$lat_max."]];\n";
 ?>
-    </script>
+
+</script>
 
 <?php
 echo "<hr><center><table>\n";
 for ($i = 0; $i < $count; $i++)
 {
-	echo "<tr><td><A HREF=\"data/".$who[$i]."/log/\">".$who[$i]."</A></td><td>@</td><td>".$locdate[$i]."</td><td>".$loctime[$i]."</td><td>(".$speed[$i].")</td></tr>\n";
+	echo "<tr><td><A HREF=\"show_history.php?who=".$who[$i]."&showday=".$today."\">".$who[$i]."</A></td><td>@</td><td>".$locdate[$i]."</td><td>".$loctime[$i]."</td><td>(".$speed[$i].")</td></tr>\n";
 }
 echo "</table></center><hr>\n";
 ?>
     <div id="mapdiv" style="width: 100%; height: 90%; background: grey" />      
     <script  type="text/javascript" charset="UTF-8" >
-        var latavg = lat[0];
-        var lonavg = lon[0];
-        for(i = 1; i < who.length; i++)
-        {
-            latavg = (latavg + lat[i]) / 2;
-            lonavg = (lonavg + lon[i]) / 2;
-        }
         
         const map = new maplibregl.Map({
             style: 'https://tiles.openfreemap.org/styles/liberty',
             center: [lonavg, latavg],
             zoom: zoomlevel,
             container: 'mapdiv',
-        })        
+            attributionControl: false,
+        });
+        
+        map.addControl(new maplibregl.NavigationControl({
+            visualizePitch: true,
+            visualizeRoll: true,
+            showZoom: true,
+            showCompass: true
+        }));
+
+        map.addControl(new maplibregl.ScaleControl({
+            maxWidth: 200,
+            unit: 'metric'
+        }));        
+
+        if(cnt > 1)
+        {
+            map.fitBounds(bbox, { padding: 100, animate: false, maxZoom:15});
+        };
 
         // show the requested users
         for(i = 0; i < who.length; i++)
@@ -136,7 +181,8 @@ echo "</table></center><hr>\n";
           new maplibregl.Marker({element: el})
                 .setLngLat([ lon[i], lat[i] ])
                 .addTo(map);        
-         };
+        };
+
     </script>
   </body>
 </html>
